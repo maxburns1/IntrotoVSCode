1be2d14259d92552db615350de3606c5d4d789ae
Max Burns <max_burns1@baylor.edu>
2026-04-08 16:16:39 -0500

    updated commit

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    # Explicitly define logout before the include to override any defaults
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    # Registration page
     path('register/', views.register, name='register'),
+    
+    # Include default auth urls (login, etc.)
+    path('', include('django.contrib.auth.urls')),
 ]
